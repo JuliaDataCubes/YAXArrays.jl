edfc3aede40b3135976cf2474affa866cc6e3f35
Fabian Gans <fgans@bgc-jena.mpg.de>
2015-12-04 18:25:18 +0100

    added some doc strings

--- src/DataCubeReader.jl
+++ src/DataCubeReader.jl
@@ -1,3 +1,9 @@
+"""
+## The Earth System Data Cube
+![](http://earthsystemdatacube.net/wp-content/uploads/2015/07/EarthDataCube3.png "The DataCube")
+
+Some info on the project...
+"""
 module DataCubeReader
 export Cube, CubeData, getCube,getTimeRanges
 
@@ -8,6 +14,27 @@ type ConfigEntry{LHS}
     lhs
     rhs
 end
+
+
+"
+ A data cube's static configuration information.
+
+ - `spatial_res`: The spatial image resolution in degree.
+ - `grid_x0`: The fixed grid X offset (longitude direction).
+ - `grid_y0`: The fixed grid Y offset (latitude direction).
+ - `grid_width`: The fixed grid width in pixels (longitude direction).
+ - `grid_height`: The fixed grid height in pixels (latitude direction).
+ - `temporal_res`: The temporal resolution in days.
+ - `ref_time`: A datetime value which defines the units in which time values are given, namely days since *ref_time*.
+ - `start_time`: The start time of the first image of any variable in the cube given as datetime value.
+                    ``None`` means unlimited.
+ - `end_time`: The end time of the last image of any variable in the cube given as datetime value.
+                  ``None`` means unlimited.
+ - `variables`: A list of variable names to be included in the cube.
+ - `file_format`: The file format used. Must be one of 'NETCDF4', 'NETCDF4_CLASSIC', 'NETCDF3_CLASSIC'
+                     or 'NETCDF3_64BIT'.
+ - `compression`: Whether the data should be compressed.
+ "
 type CubeConfig
     end_time::DateTime
     ref_time::DateTime
@@ -48,6 +75,13 @@ function parseConfig(cubepath)
   d
 end
 
+"
+Represents a data cube. The default constructor is
+
+    Cube(base_dir)
+
+where `base_dir` is the datacube's base directory.
+"
 type Cube
     base_dir::UTF8String
     config::CubeConfig
@@ -60,6 +94,12 @@ type CubeData
     var_name_to_var_index::OrderedDict{UTF8String,Int}
     firstYearOffset::Int
 end
+
+"""
+CubeData(Cube::Cube)
+
+Returns a representation of the cube's read-only data. This can be passed to further getCube function calls.
+"""
 function CubeData(cube::Cube)
     data_dir=joinpath(cube.base_dir,"data")
     data_dir_entries=readdir(data_dir)
@@ -70,6 +110,21 @@ function CubeData(cube::Cube)
     CubeData(cube,data_dir_entries,var_name_to_var_index,firstYearOffset)
 end
 
+"""
+
+    Cube(cubedata::CubeData;variable,time,latitude,longitude)
+
+The following keyword arguments are accepted:
+
+- *variable*: an variable index or name or an iterable returning multiple of these (var1, var2, ...)
+- *time*: a single datetime.datetime object or a 2-element iterable (time_start, time_end)
+- *latitude*: a single latitude value or a 2-element iterable (latitude_start, latitude_end)
+- *longitude*: a single longitude value or a 2-element iterable (longitude_start, longitude_end)
+
+Returns a dictionary mapping variable names --> arrays of dimension (longitude, latitude, time)
+
+http://earthsystemdatacube.org
+"""
 function getCube(cubedata::CubeData;variable=Int[],time=[],latitude=[],longitude=[])
     #First fill empty inputs
     isempty(variable) && (variable = cubedata.dataset_files)
@@ -83,6 +138,7 @@ end
 
 using NetCDF
 vartype{T,N}(v::NcVar{T,N})=T
+"Function to get the years and times to read from user input."
 function getTimesToRead(time1,time2,config)
     NpY    = ceil(Int,365/config.temporal_res)
     y1     = year(time1)
@@ -95,6 +151,7 @@ function getTimesToRead(time1,time2,config)
     return y1,index1,y2,index2,ntimesteps,NpY
 end
 
+"Returns a vector of DateTime objects giving the time indices returned by a respective call to getCube."
 function getTimeRanges(cdata::CubeData,time::Tuple{Dates.TimeType,Dates.TimeType}=(cdata.cube.config.start_time,cdata.cube.config.end_time-Dates.Day(1)))
     c=cdata.cube
     NpY    = ceil(Int,365/c.config.temporal_res)
@@ -102,6 +159,38 @@ function getTimeRanges(cdata::CubeData,time::Tuple{Dates.TimeType,Dates.TimeType
     #TODO handle non-full year case properly
     reshape([DateTime(y)+Dates.Day((i-1)*c.config.temporal_res) for i=1:NpY, y=yrange],NpY*length(yrange))
 end
+
+#Convert single input to vectors
+function getCube{T<:Union{Integer,AbstractString}}(cubedata::CubeData,
+                variable::Union{AbstractString,Integer,AbstractVector{T}},
+                time::Union{Tuple{TimeType,TimeType},TimeType},
+                latitude::Union{Tuple{Real,Real},Real},
+                longitude::Union{Tuple{Real,Real},Real})
+
+  isa(time,TimeType) && (time=(time,time))
+  isa(latitude,Real) && (latitude=(latitude,latitude))
+  isa(longitude,Real) && (longitude=(longitude,longitude))
+  isa(variable,AbstractVector) && isa(eltype(variable),Integer) && (variable=[cubedata.cube.config.dataset_files[i] for i in variable])
+  isa(variable,Integer) && (variable=dataset_files[i])
+  getCube(cubedata,variable,time,longitude,latitude)
+end
+
+function getCube{T<:AbstractString}(cubedata::CubeData,
+                variable::Vector{T},
+                time::Tuple{TimeType,TimeType},
+                latitude::Tuple{Real,Real},
+                longitude::Tuple{Real,Real})
+  r=Dict{T,Any}()
+  for i=1:length(variable)
+    if haskey(cubedata.var_name_to_var_index,variable[i])
+      r[variable[i]]=getCube(cubedata,variable[i],time,latitude,longitude)
+    else
+      warn("Skipping variable $(variable[i]), not found in Datacube")
+    end
+  end
+end
+
+
 
 function getCube(cubedata::CubeData,
                 variable::AbstractString,
