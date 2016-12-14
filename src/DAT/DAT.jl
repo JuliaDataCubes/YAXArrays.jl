a39556f6f360514eaa22b19d03df6c3198a13e58
Fabian Gans <fabiangans@gmx.de>
2016-12-14 14:38:18 +0100

    Remove debug print info

--- src/DAT/DAT.jl
+++ src/DAT/DAT.jl
@@ -12,7 +12,7 @@ import NullableArrays.NullableArray
 import NullableArrays.isnull
 import StatsBase.WeightVec
 importall CABLAB.CubeAPI.Mask
-global const debugDAT=true
+global const debugDAT=false
 macro debug_print(e)
   debugDAT && return(:(println($e)))
   :()
