88f07c86af05df8b83dbdf3d1a7adf4fdaf7ad94
meggart <fabiangans@gmx.de>
2018-06-11 09:44:49 +0200

    move plotHist to ESDLPlots

--- src/Proc/Online/OnlineHist.jl
+++ src/Proc/Online/OnlineHist.jl
@@ -2,19 +2,23 @@ struct HistogramCube{T<:AbstractCubeData}
   c::T
 end
 
-plotHist(c::HistogramCube)=plotScatter(tohist(c.c),vsaxis="Hist",xaxis="MidPoints",yaxis="Frequency",alongaxis="Bin")
-
 function tohist(d::HistogramCube)
   c=d.c
   nbins=length(OnlineStats.value(c.data[1])[1])
   cout=zeros(Float32,nbins,2,size(c.data)...)
   maskout=zeros(UInt8,nbins,2,size(c.data)...)
   for ii in CartesianRange(size(c.data))
-    midp,v = OnlineStats.value(c.data[ii])
-    cout[:,1,ii]=v
-    cout[:,2,ii]=midp
-    maskout[:,1,ii]=c.mask[ii]
-    maskout[:,2,ii]=c.mask[ii]
+    if OnlineStats.nobs(c.data[ii])>0
+      midp,v = OnlineStats.value(c.data[ii])
+      cout[:,1,ii]=v
+      cout[:,2,ii]=midp
+      maskout[:,1,ii]=c.mask[ii]
+      maskout[:,2,ii]=c.mask[ii]
+    else
+      cout[:,1,ii]=1:size(cout,1)
+      cout[:,2,ii]=1:size(cout,1)
+      maskout[:,:,ii]=0x01
+    end
   end
   classAx=CategoricalAxis("Bin",1:nbins)
   histAx=CategoricalAxis("Histogram",["Frequency","MidPoints"])
