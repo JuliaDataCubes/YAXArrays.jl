727736a7e9cf9e93710811018d7f780108b9f2c1
Fabian Gans <fgans@bgc-jena.mpg.de>
2019-05-02 09:30:46 +0200

    Should have running tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using ESDL
 using Test
 #Make sure S3 cube is always accessed for tests
+using Pkg
+Pkg.add(PackageSpec(name="AWSCore", rev="master"))
+
 delete!(ENV,"ESDL_CUBEDIR")
 
 include("access.jl")
