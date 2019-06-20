fbc963ab958b1e5c2a3a5e0182326dd96a5487ca
Fabian Gans <fgans@floriana.speedport_w_723v_1_46_000>
2019-06-20 22:45:15 +0200

    remove webio again

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
-using ImageMagick, Documenter, ESDL, ESDLPlots, Cairo, Fontconfig, WebIO
+using ImageMagick, Documenter, ESDL, ESDLPlots, Cairo, Fontconfig
 
 makedocs(
     modules = [ESDL, ESDLPlots],
