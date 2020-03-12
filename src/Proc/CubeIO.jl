4a6f503d58e9bdcb382aa8fe05f207de7d6a9452
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-03-12 13:19:32 +0100

    Add sync after exporting cube

--- src/Proc/CubeIO.jl
+++ src/Proc/CubeIO.jl
@@ -2,7 +2,7 @@ module CubeIO
 
 using Base.Iterators: Iterators, product
 using Zarr: ZArray, NoCompressor
-using NetCDF: ncwrite, NcDim, NcVar, ncread, create, nccreate, ncputatt
+using NetCDF: ncwrite, NcDim, NcVar, ncread, create, nccreate, ncputatt, sync
 export exportcube, extractLonLats
 import ...Cubes: saveCube, check_overwrite, getsavefolder, cubechunks, AbstractCubeData, caxes,
   axname
@@ -101,6 +101,7 @@ function exportcube(r::AbstractCubeData,filename::String;priorities = Dict("LON"
   cont_loop = Dict(ii=>axname(ax_cont[ii]) for ii in isplit:length(ax_cont))
   mapCube(writefun,r,length(ax_cont),cont_loop,filename,indims=incubes,include_loopvars=true,ispar=false,max_cache=5e8,
   nthreads=[1])
+  sync(file)
   nothing
 end
 
