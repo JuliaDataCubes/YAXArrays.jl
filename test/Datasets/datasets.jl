2e764cc1eff771629fe703b32ac8982a0cb13ace
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-08-14 17:57:22 +0200

    test dim collection

--- test/Datasets/datasets.jl
+++ test/Datasets/datasets.jl
@@ -46,4 +46,53 @@
     @test collect(values(ds3.cubes)) == [c1,c2]
     @test ds["avar"] === c1
   end
+  @testset "Dataset interface" begin
+    struct MockDataset
+      vars
+      dims
+      attrs
+      path
+    end
+    Base.getindex(d::MockDataset,i) = d.vars[i]
+    Base.haskey(d::MockDataset,i) = haskey(d.vars,i)
+    YAXArrayBase.get_varnames(d::MockDataset) = (keys(d.vars)...,)
+    YAXArrayBase.get_var_dims(d::MockDataset,name) = d.dims[name]
+    YAXArrayBase.get_var_attrs(d::MockDataset, name) = d.attrs[name]
+    YAXArrayBase.create_empty(::Type{MockDataset},path) = MockDataset(Dict(),Dict(),Dict(),path)
+    function YAXArrayBase.add_var(ds::MockDataset, T, name, s, dimlist, atts;kwargs...)
+      ds.vars[name] = zeros(T,s...)
+      ds.dims[name] = dimlist
+      ds.atts[name] = atts
+    end
+    YAXArrayBase.backendlist[:mock] = MockDataset
+    push!(YAXArrayBase.backendregex,r".mock$"=>MockDataset)
+    data1,data2,d1,d2,d3 = (rand(12,5,2),rand(12,5),1:12, 0.1:0.1:0.5, ["One","Two"])
+    att1 =  Dict("att1"=>5,"_ARRAY_OFFSET"=>(2,0,0))
+    att2 =  Dict("att2"=>6,"_ARRAY_OFFSET"=>(2,0))
+    attd1 = Dict("_ARRAY_OFFSET"=>2, "units"=>"days since 2001-01-01", "calendar"=>"gregorian")
+    attd2 = Dict("attd"=>"d")
+    attd3 = Dict("attd"=>"d")
+    m = MockDataset(
+    Dict("Var1"=>data1, "Var2"=>data2, "time"=>d1,"d2"=>d2, "d3"=>d3),
+    Dict("Var1"=>("time","d2","d3"),"Var2"=>("time","d2"),"time"=>("time",),"d2"=>["d2"],"d3"=>["d3"]),
+    Dict("Var1"=>att1,"Var2"=>att2,"time"=>attd1,"d2"=>attd2,"d3"=>attd3),
+    "testpath.mock"
+    )
+    @testset "collectdims" begin
+      dcollect = YAXArrays.Datasets.collectdims(m)
+      @test dcollect["time"].ax isa RangeAxis
+      @test YAXArrays.Cubes.Axes.axname(dcollect["time"].ax) == "time"
+      @test dcollect["time"].ax.values == DateTime(2001,1,4):Day(1):DateTime(2001,1,13)
+      @test dcollect["time"].offs == 2
+      @test dcollect["d2"].ax isa RangeAxis
+      @test YAXArrays.Cubes.Axes.axname(dcollect["d2"].ax) == "d2"
+      @test dcollect["d2"].ax.values == 0.1:0.1:0.5
+      @test dcollect["d2"].offs == 0
+      @test dcollect["d3"].ax isa CategoricalAxis
+      @test YAXArrays.Cubes.Axes.axname(dcollect["d3"].ax) == "d3"
+      @test dcollect["d3"].ax.values == ["One","Two"]
+      @test dcollect["d3"].offs == 0
+    end
+
+  end
 end
