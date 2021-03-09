52a9d187879637cfcfc8b3f48e8440d7a47b0c9f
Fabian Gans <fgans@bgc-jena.mpg.de>
2021-03-09 17:30:55 +0100

    Depend on Reexport again

--- src/YAXTools.jl
+++ src/YAXTools.jl
@@ -131,30 +131,4 @@ macro loadOrGenerate(x...)
   end)
 end
 
-# Here we define ouur own reexport macro copied from
-# https://github.com/simonster/Reexport.jl/blob/master/src/Reexport.jl
-
-macro reexport(ex)
-    isa(ex, Expr) && (ex.head == :module ||
-                      ex.head == :using ||
-                      (ex.head == :toplevel &&
-                       all(e->isa(e, Expr) && e.head == :using, ex.args))) ||
-        error("@reexport: syntax error")
-
-    if ex.head == :module
-        modules = Any[ex.args[2]]
-        ex = Expr(:toplevel, ex, :(using .$(ex.args[2])))
-    elseif ex.head == :using && all(e->isa(e, Symbol), ex.args)
-        modules = Any[ex.args[end]]
-    elseif ex.head == :using && ex.args[1].head == :(:)
-        symbols = [e.args[end] for e in ex.args[1].args[2:end]]
-        return esc(Expr(:toplevel, ex, :(eval(Expr(:export, $symbols...)))))
-    else
-        modules = Any[e.args[end] for e in ex.args]
-    end
-
-    esc(Expr(:toplevel, ex,
-             [:(eval(Expr(:export, names($mod)...))) for mod in modules]...))
-end
-
 end
