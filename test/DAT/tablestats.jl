338dc1d527597c1f076fc851a0919e500b2059ba
Felix Cremer <felix.cremer@uni-jena.de>
2021-07-01 10:50:41 +0200

    Add first tests for cubefittable (#55)
    * Add OnlineStats as test dependency
    
    * Add first test for cubefittable
    
    I changed MovingWindow to YAXArrays.MovingWindow in the tests,
    because it is shadowed in OnlineStats.
    Otherwise the tests would depend on the inclusion order in runtests.jl.
    
    Co-authored-by: Fabian Gans <fabiancoding@gmail.com>

--- test/DAT/tablestats.jl
+++ test/DAT/tablestats.jl
@@ -7,6 +7,7 @@ axlist = [RangeAxis("XVals",1.0:4.0), CategoricalAxis("YVals",[1,2,3,4,5])]
 props = Dict("att1"=>5, "att2"=>"Hallo")
 a = YAXArray(axlist, data, props)
 
+
 cta = CubeTable( data=a, include_axes=("XVals", "YVals"))
 meancta = cubefittable(cta,Mean(),:data, by=(:YVals,))
 @test meancta.data == [2.5, 6.5, 10.5, 14.5, 18.5] 
