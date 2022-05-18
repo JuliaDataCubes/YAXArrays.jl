064ded800d1bf89902addf9686279ef9a56cb03a
Martin Reinhardt <martin.reinhardt.87@gmail.com>
2022-05-18 10:34:38 +0200

    codestyling in axes.jl

--- src/Cubes/Axes.jl
+++ src/Cubes/Axes.jl
@@ -2,11 +2,17 @@ module Axes
 import ..Cubes: caxes, Cubes
 using Dates
 using Base.Iterators: take, drop
+import DataStructures: counter
+using YAXArrayBase: YAXArrayBase
 
 export CubeAxis, RangeAxis, CategoricalAxis, getAxis
 
+################################
+# Types
+################################
+
 """
-    abstract CubeAxis{T}
+    abstract type CubeAxis{T,S}
 
 Supertype of all axes. Every `CubeAxis` is an 1D Cube itself and can be passed
 to mapCube operations.
@@ -19,10 +25,8 @@ Base.size(x::CubeAxis, i) =
 Base.ndims(x::CubeAxis) = 1
 Base.hash(ax::CubeAxis{<:Any,S}, h::UInt) where {S} = hash(S, hash(ax.values, h))
 
-
-
 """
-    CategoricalAxis{T,S}
+    struct CategoricalAxis{T,S,RT}
 
 To represent axes that are categorical, where `T` is the element type.
 The type parameter `S` denotes the axis name (a symbol).
@@ -38,47 +42,6 @@ end
 CategoricalAxis(s::Symbol, v) = CategoricalAxis{eltype(v),s,typeof(v)}(v)
 CategoricalAxis(s::AbstractString, v) = CategoricalAxis(Symbol(s), v)
 
-struct RangeAxis{T,S,R<:AbstractVector{T}} <: CubeAxis{T,S}
-    values::R
-end
-
-"""
-    RangeAxis{T,S,R}
-
-To represent axes that are categorical, where `T` is the element type.
-The type parameter `S` denotes the axis name (a symbol) and `R` the type of the
-range which is used to represent the axis values.
-The default constructor is:
-
-    RangeAxis(axname::String,values::Range{T})
-
-"""
-RangeAxis(s::Symbol, v::AbstractVector{T}) where {T} = RangeAxis{T,s,typeof(v)}(v)
-RangeAxis(s::AbstractString, v) = RangeAxis(Symbol(s), v)
-
-Base.length(a::CubeAxis) = length(a.values)
-
-"""
-    axcopy(x,vals)
-
-Makes a copy of a `CubeAxis` with the values `vals`
-"""
-axcopy(ax::RangeAxis, vals) = RangeAxis(axname(ax), vals)
-axcopy(ax::CategoricalAxis, vals) = CategoricalAxis(axname(ax), vals)
-axcopy(ax::RangeAxis) = RangeAxis(axname(ax), copy(ax.values))
-axcopy(ax::CategoricalAxis) = CategoricalAxis(axname(ax), copy(ax.values))
-
-
-Base.show(io::IO, a::RangeAxis) = print(
-    io,
-    rpad(Axes.axname(a), 20, " "),
-    "Axis with ",
-    length(a),
-    " Elements from ",
-    first(a.values),
-    " to ",
-    last(a.values),
-)
 function Base.show(io::IO, a::CategoricalAxis)
     print(io, rpad(Axes.axname(a), 20, " "), "Axis with ", length(a), " elements: ")
     if length(a.values) < 10
@@ -96,49 +59,141 @@ function Base.show(io::IO, a::CategoricalAxis)
     end
 end
 
-caxes(x::CubeAxis) = CubeAxis[x]
+"""
+    RangeAxis{T,S,R}
 
+To represent axes that are numerical, where `T` is the element type.
+The type parameter `S` denotes the axis name (a symbol) and `R` the type of the
+range which is used to represent the axis values.
+The default constructor is:
+
+    RangeAxis(axname::String,values::Range{T})
+
+"""
+struct RangeAxis{T,S,R<:AbstractVector{T}} <: CubeAxis{T,S}
+    values::R
+end
+
+RangeAxis(s::Symbol, v::AbstractVector{T}) where {T} = RangeAxis{T,s,typeof(v)}(v)
+RangeAxis(s::AbstractString, v) = RangeAxis(Symbol(s), v)
+
+Base.length(a::CubeAxis) = length(a.values)
+
+Base.show(io::IO, a::RangeAxis) = print(
+    io,
+    rpad(Axes.axname(a), 20, " "),
+    "Axis with ",
+    length(a),
+    " Elements from ",
+    first(a.values),
+    " to ",
+    last(a.values),
+)
+
+abstract type AxisDescriptor end
+
+struct ByName <: AxisDescriptor
+    name::String
+end
+
+struct ByInference <: AxisDescriptor end
+
+struct ByValue <: AxisDescriptor
+    v::CubeAxis
+end
+
+const VecOrTuple{S} = Union{Vector{<:S},Tuple{Vararg{<:S}}} where {S}
+
+################################
+# Public
+################################
+
+import Base.==
+import Base.isequal
+==(a::CubeAxis, b::CubeAxis) = (a.values == b.values) && (axname(a) == axname(b))
+isequal(a::CubeAxis, b::CubeAxis) = a == b
+
+"""
+    getAxis(desc, c)
+
+Given an Axis description and a cube, returns the corresponding axis of the cube.
+The Axis description can be:
+  - the name as a string or symbol.
+  - an Axis object
+"""
+getAxis(desc, c) = getAxis(desc, caxes(c))
+getAxis(desc::ByValue, axlist::Vector{T}) where {T<:CubeAxis} = desc.v
+function getAxis(desc, axlist::VecOrTuple{CubeAxis})
+    i = findAxis(desc, axlist)
+    if isa(i, Nothing)
+        return nothing
+    else
+        return axlist[i]
+    end
+end
+
+#Implement yaxarray interface
+YAXArrayBase.dimname(x::CubeAxis, _) = axname(x)
+YAXArrayBase.dimvals(x::CubeAxis, _) = x.values
+YAXArrayBase.iscontdim(::RangeAxis, _) = true
+YAXArrayBase.iscontdim(::CategoricalAxis, _) = false
+
+################################
+# Utility functions (internal)
+################################
+
+"""
+    axname
+"""
 axname(::Type{<:CubeAxis{<:Any,U}}) where {U} = string(U)
 axname(::CubeAxis{<:Any,U}) where {U} = string(U)
+
+"""
+    axsym
+"""
 axsym(::CubeAxis{<:Any,S}) where {S} = S
 
+"""
+    axcopy(x,vals)
+
+Makes a full copy of a `CubeAxis` with the values `vals`
+"""
+axcopy(ax::RangeAxis, vals) = RangeAxis(axname(ax), vals)
+axcopy(ax::CategoricalAxis, vals) = CategoricalAxis(axname(ax), vals)
+axcopy(ax::RangeAxis) = RangeAxis(axname(ax), copy(ax.values))
+axcopy(ax::CategoricalAxis) = CategoricalAxis(axname(ax), copy(ax.values))
+
+"""
+    caxes
+
+Embeds  Cube inside a new Cube
+"""
+caxes(x::CubeAxis) = CubeAxis[x]
+
+"""
+    get_step
+
+returns stepwidth of the RangeAxis
+"""
 get_step(r::AbstractRange) = step(r)
 get_step(r::AbstractVector) = length(r) == 0 ? zero(eltype(r)) : r[2] - r[1]
 
-axVal2Index_ub(a::RangeAxis, v; fuzzy = false) =
-    axVal2Index(a, v - abshalf(get_step(a.values)), fuzzy = fuzzy)
-axVal2Index_lb(a::RangeAxis, v; fuzzy = false) =
-    axVal2Index(a, v + abshalf(get_step(a.values)), fuzzy = fuzzy)
-
-axVal2Index_ub(a::RangeAxis, v::Date; fuzzy = false) =
-    axVal2Index(a, DateTime(v) - abshalf(get_step(a.values)), fuzzy = fuzzy)
-axVal2Index_lb(a::RangeAxis, v::Date; fuzzy = false) =
-    axVal2Index(a, DateTime(v) + abshalf(get_step(a.values)), fuzzy = fuzzy)
-
+"""
+    abshalf
+"""
 abshalf(a) = abs(a / 2)
 abshalf(a::Day) = abs(Millisecond(a) / 2)
 abshalf(a::Month) = iseven(Dates.value(a)) ? a / 2 : Month(a ÷ 2) + Day(15)
 
-get_bb(ax::RangeAxis) = first(ax.values) - abshalf(get_step(ax.values)),
-last(ax.values) + abshalf(get_step(ax.values))
-function axisfrombb(name, bb, n)
-    offs = (bb[2] - bb[1]) / (2 * n)
-    RangeAxis(name, range(bb[1] + offs, bb[2] - offs, length = n))
-end
-
-function axVal2Index(a::RangeAxis{<:Any,<:Any,<:AbstractRange}, v; fuzzy = false)
+"""
+    axVal2Index
+"""
+function axVal2Index(a::RangeAxis{<:Any,<:Any,<:AbstractRange}, v; fuzzy=false)
     dt = v - first(a.values)
     r = round(Int, dt / step(a.values)) + 1
     return max(1, min(length(a.values), r))
 end
-
-convert_time(T::Type{<:TimeType}, v::TimeType) =
-    T(year(v), month(v), day(v), hour(v), minute(v), second(v))
-convert_time(T::Type{<:TimeType}, v::Date) = T(year(v), month(v), day(v), 0, 0, 0)
-convert_time(::Type{Date}, v::TimeType) = Date(year(v), month(v), day(v))
-convert_time(::Type{Date}, v::Date) = Date(year(v), month(v), day(v))
-
-function axVal2Index(a::RangeAxis{T}, v; fuzzy = false) where {T<:TimeType}
+function axVal2Index(a::RangeAxis{T}, v; fuzzy=false) where {T<:TimeType}
     vconverted = convert_time(T, v)
     dd = map(i -> abs((i - vconverted)), a.values)
     mi, ind = findmin(dd)
@@ -147,14 +202,14 @@ end
 function axVal2Index(
     a::RangeAxis{T,<:Any,<:AbstractRange},
     v;
-    fuzzy = false,
+    fuzzy=false
 ) where {T<:TimeType}
     vconverted = convert_time(T, v)
     dd = map(i -> abs((i - vconverted)), a.values)
     mi, ind = findmin(dd)
     return ind
 end
-function axVal2Index(axis::CategoricalAxis{String}, v::String; fuzzy::Bool = false)
+function axVal2Index(axis::CategoricalAxis{String}, v::String; fuzzy::Bool=false)
     r = findfirst(isequal(v), axis.values)
     if r === nothing
         if fuzzy
@@ -172,8 +227,8 @@ function axVal2Index(axis::CategoricalAxis{String}, v::String; fuzzy::Bool = fal
     end
     r
 end
-axVal2Index(x, v::CartesianIndex{1}; fuzzy::Bool = false) = min(max(v.I[1], 1), length(x))
-function axVal2Index(x, v; fuzzy::Bool = false)
+axVal2Index(x, v::CartesianIndex{1}; fuzzy::Bool=false) = min(max(v.I[1], 1), length(x))
+function axVal2Index(x, v; fuzzy::Bool=false)
     i = findfirst(isequal(v), x.values)
     if isa(i, Nothing)
         if fuzzy == true
@@ -187,19 +242,47 @@ function axVal2Index(x, v; fuzzy::Bool = false)
         return i
     end
 end
-abstract type AxisDescriptor end
-struct ByName <: AxisDescriptor
-    name::String
-end
-struct ByInference <: AxisDescriptor end
-struct ByValue <: AxisDescriptor
-    v::CubeAxis
-end
-
-const VecOrTuple{S} = Union{Vector{<:S},Tuple{Vararg{<:S}}} where {S}
 
 """
-# Internal 
+    axVal2Index_ub
+"""
+axVal2Index_ub(a::RangeAxis, v; fuzzy=false) =
+    axVal2Index(a, v - abshalf(get_step(a.values)), fuzzy=fuzzy)
+axVal2Index_ub(a::RangeAxis, v::Date; fuzzy=false) =
+    axVal2Index(a, DateTime(v) - abshalf(get_step(a.values)), fuzzy=fuzzy)
+
+"""
+    axVal2Index_lb
+"""
+axVal2Index_lb(a::RangeAxis, v; fuzzy=false) =
+    axVal2Index(a, v + abshalf(get_step(a.values)), fuzzy=fuzzy)
+axVal2Index_lb(a::RangeAxis, v::Date; fuzzy=false) =
+    axVal2Index(a, DateTime(v) + abshalf(get_step(a.values)), fuzzy=fuzzy)
+
+"""
+    get_bb
+"""
+get_bb(ax::RangeAxis) = (first(ax.values) - abshalf(get_step(ax.values)),
+    last(ax.values) + abshalf(get_step(ax.values)))
+
+"""
+    axisfrombb
+"""
+function axisfrombb(name, bb, n)
+    offs = (bb[2] - bb[1]) / (2 * n)
+    RangeAxis(name, range(bb[1] + offs, bb[2] - offs, length=n))
+end
+
+"""
+    convert_time
+"""
+convert_time(T::Type{<:TimeType}, v::TimeType) =
+    T(year(v), month(v), day(v), hour(v), minute(v), second(v))
+convert_time(T::Type{<:TimeType}, v::Date) = T(year(v), month(v), day(v), 0, 0, 0)
+convert_time(::Type{Date}, v::TimeType) = Date(year(v), month(v), day(v))
+convert_time(::Type{Date}, v::Date) = Date(year(v), month(v), day(v))
+
+"""
     get_descriptor(a)
 
 Get the descriptor of an Axis. 
@@ -211,18 +294,26 @@ get_descriptor(a::CubeAxis) = ByValue(a)
 get_descriptor(a) = error("$a is not a valid axis description")
 get_descriptor(a::AxisDescriptor) = a
 
-match_axis(a, ax) = match_axis(get_descriptor(a), ax)
-
+"""
+    match_axis
+"""
 function match_axis(bs::ByName, ax)
     startswith(lowercase(axname(ax)), lowercase(bs.name))
 end
-
 function match_axis(bs::ByValue, ax)
     isequal(bs.v, ax)
 end
+match_axis(a, ax) = match_axis(get_descriptor(a), ax)
 
-
-
+"""
+    findAxis(desc, c)
+Given an Axis description and a cube return the index of the Axis.
+The Axis description can be:
+  - the name as a string or symbol.
+  - an Axis object
+"""
+findAxis(desc, c) = findAxis(desc, caxes(c))
+findAxis(a, axlist::VecOrTuple{CubeAxis}) = findAxis(get_descriptor(a), axlist)
 function findAxis(bs::AxisDescriptor, axlist::VecOrTuple{CubeAxis})
     m = findall(i -> match_axis(bs, i), axlist)
     if isempty(m)
@@ -234,16 +325,18 @@ function findAxis(bs::AxisDescriptor, axlist::VecOrTuple{CubeAxis})
     end
 end
 
-function getAxis(desc, axlist::VecOrTuple{CubeAxis})
-    i = findAxis(desc, axlist)
-    if isa(i, Nothing)
-        return nothing
-    else
-        return axlist[i]
-    end
-end
+"""
+    renameaxis
+"""
+renameaxis(r::RangeAxis{T,<:Any,V}, newname) where {T,V} =
+    RangeAxis{T,Symbol(newname),V}(r.values)
+renameaxis(r::CategoricalAxis{T,<:Any,V}, newname) where {T,V} =
+    CategoricalAxis{T,Symbol(newname),V}(r.values)
+
+"""
+    getOutAxis
+"""
 getOutAxis(desc, axlist, incubes, pargs, f) = getAxis(desc, unique(axlist))
-import DataStructures: counter
 function getOutAxis(desc::Tuple{ByInference}, axlist, incubes, pargs, f)
     inAxes = map(caxes, incubes)
     inAxSmall = map(i -> filter(j -> in(j, axlist), i) |> collect, inAxes)
@@ -283,41 +376,4 @@ function getOutAxis(desc::Tuple{ByInference}, axlist, incubes, pargs, f)
     return (outaxes...,)
 end
 
-"""
-    getAxis(desc, c)
-
-Given an Axis description and a cube, returns the corresponding axis of the cube.
-The Axis description can be:
-  - the name as a string or symbol.
-  - an Axis object
-"""
-getAxis(desc, c) = getAxis(desc, caxes(c))
-getAxis(desc::ByValue, axlist::Vector{T}) where {T<:CubeAxis} = desc.v
-
-"""
-    findAxis(desc, c)
-Given an Axis description and a cube return the index of the Axis.
-The Axis description can be:
-  - the name as a string or symbol.
-  - an Axis object
-"""
-findAxis(desc, c) = findAxis(desc, caxes(c))
-findAxis(a, axlist::VecOrTuple{CubeAxis}) = findAxis(get_descriptor(a), axlist)
-
-renameaxis(r::RangeAxis{T,<:Any,V}, newname) where {T,V} =
-    RangeAxis{T,Symbol(newname),V}(r.values)
-renameaxis(r::CategoricalAxis{T,<:Any,V}, newname) where {T,V} =
-    CategoricalAxis{T,Symbol(newname),V}(r.values)
-
-import Base.==
-import Base.isequal
-==(a::CubeAxis, b::CubeAxis) = (a.values == b.values) && (axname(a) == axname(b))
-isequal(a::CubeAxis, b::CubeAxis) = a == b
-
-using YAXArrayBase: YAXArrayBase
-#Implement yaxarray interface
-YAXArrayBase.dimname(x::CubeAxis, _) = axname(x)
-YAXArrayBase.dimvals(x::CubeAxis, _) = x.values
-YAXArrayBase.iscontdim(::RangeAxis, _) = true
-YAXArrayBase.iscontdim(::CategoricalAxis, _) = false
-end
+end #module
