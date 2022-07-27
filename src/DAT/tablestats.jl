68f7372cc5dcd0eba5d1cb95427fc4bda19265de
Felix Cremer <fcremer@bgc-jena.mpg.de>
2022-07-27 11:12:40 +0200

    Add Cube docstring

--- src/DAT/tablestats.jl
+++ src/DAT/tablestats.jl
@@ -318,7 +318,7 @@ fitfun(::Type{<:Any}) = fittable
 """
     cubefittable(tab,o,fitsym;post=getpostfunction(o),kwargs...)
 
-Executes [`fittable`](@ref) on the [`@CubeTable`](@ref) `tab` with the
+Executes [`fittable`](@ref) on the [`CubeTable`](@ref) `tab` with the
 (Weighted-)OnlineStat `o`, looping through the values specified by `fitsym`.
 Finally, writes the results from the `TableAggregator` to an output data cube.
 """
