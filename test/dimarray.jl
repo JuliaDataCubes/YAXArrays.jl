8cb3e4ff356207514196d1f2958f9969c6a43601
Felix Cremer <fcremer@bgc-jena.mpg.de>
2022-09-28 15:47:07 +0200

    Copy more yaxarray test cases into DimArray tests

--- test/dimarray.jl
+++ test/dimarray.jl
@@ -19,5 +19,44 @@
     r = mapCube(c, indims=indims, outdims=outdims) do xout, xin
         xout .= sum(skipmissing(xin))
     end
+    anymiss = mapCube(c, indims=InDims("Time", filter=YAXArrays.DAT.AnyMissing()), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test ismissing(anymiss[1,1])
     @test r[1,1] == 52
+    filt(x) = all(iseven.(x))
+    allevensum = mapCube(c, indims=InDims("Time", filter=filt), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test ismissing(allevensum[1,2,1])
+    
+    validmiss = mapCube(c, indims=InDims("Time", filter=YAXArrays.DAT.NValid(6)), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test ismissing(validmiss[3,1])
+
+    validcheck = mapCube(c, indims=InDims("Time", filter=YAXArrays.DAT.NValid(4)), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test validcheck[3,1] == 15
+
+    stdzero = mapCube(c, indims=InDims("Time", filter=YAXArrays.DAT.StdZero()), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test ismissing(stdzero[4,1])
+
+    nofilter = mapCube(c, indims=InDims("Time", filter=YAXArrays.DAT.NoFilter()), outdims=outdims) do xout, xin
+        xout .= sum(skipmissing(xin))
+    end
+    @test any(ismissing.(nofilter.data)) == false
+
+    @test_broken r isa DimArray
+    temp = tempname()
+    @test_broken c_saved = savecube(c, temp)
+    @test_broken c_saved isa DimArray 
+    @test_broken all(c_saved[:,:,:] .== c[:,:,:])
+
+    @test_broken c_reopened = open_dataset(temp, artype=DimArray) isa DimArray
+    @test_broken all(c_reopened.data .== c)
+    @test_broken c.dims[1] == c_reopened.dims[1]
 end
