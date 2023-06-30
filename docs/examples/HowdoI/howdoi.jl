ba58a677ddc36a4286c41174d96f0cf715c1a285
Lina Estupinan <lestup@bgc-jena.mpg.de>
2023-06-30 12:15:22 +0200

    examples for how do i

--- docs/examples/HowdoI/howdoi.jl
+++ docs/examples/HowdoI/howdoi.jl
@@ -39,7 +39,7 @@ dsfinal = concatenatecubes([ds1, ds2],
 dsfinal
 
 
-# ##  Subsetting a Cube
+# ## How do I subset a Cube?
 
 # Let's start by creating a dummy cube
 
@@ -66,12 +66,42 @@ ctime3 = c[Time=Date(2021-01-05)..Date(2021-01-12)]
 clonlat = c[Lon=1..5, Lat=5..10] # check even numbers range, it is ommiting them
 
 
-# ##  Applying map algebra
+# ##  How do I apply map algebra?
 # Our next step is map algebra computations. This can be done effectively using the 'map' function. For example:
 
-## cubes with only spatio-temporal dimensions
+## multiplying cubes with only spatio-temporal dimensions
 map((x,y)->x*y, ds1, ds2)
 
 ## cubes with more than 3 dimensions
 map((x,y)->x*y, dsfinal[Variables="Var1"], dsfinal[Variables="Var2"])
 
+# To add some complexity, we will multiply each value for π and then divided for the sum of each time step. We will use the `ds1` cube for this purpose.
+mapslices(ds1, dims=("Lon","Lat")) do xin
+    (xin*π) ./ maximum(skipmissing(xin))
+end
+
+# ## How do I use the CubeTable function?
+# The function "CubeTable" creates an iterable table and the result is a DataCube. It is therefore very handy for grouping data and computing statistics by class. It uses `OnlineStats.jl` to calculate statistics, and weighted statistics can be calculated as well.
+
+# Here we will use the `ds1` Cube  defined previously and we create a mask for data classification.
+
+## cube containing a mask with classes 1, 2 and 3
+classes = YAXArray([getAxis("lon", dsfinal), getAxis("lat", dsfinal)], rand(1:3, 10, 15))
+
+using CairoMakie
+# This is how our classification map looks like
+heatmap(classes[:,:])
+
+# Now we define the input cubes that will be considered for the iterable table
+t = CubeTable(values=ds1, classes=classes)
+
+using DataFrames
+using OnlineStats
+## visualiztion of the CubeTable
+DataFrame(t[1])
+
+# In this line we calculate the `Mean` for each class
+fitcube = cubefittable(t, Mean, :values, by=(:classes))
+
+# We can also use more than one criteria for grouping the values. In the next example, the mean is calculated for each class and timestep.
+fitcube = cubefittable(t, Mean, :values, by=(:classes,:time))
