c8f84bf91deb74840d0d35e2517eed01b3569f49
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-07-04 14:17:18 +0200

    Ignore the ESDL studies and generate from func
    These need to be switched to the new syntax later.

--- docs/genfiles.jl
+++ docs/genfiles.jl
@@ -12,7 +12,7 @@ function getfiles()
     srcsfiles = []
     for f in folders
         names = readdir(joinpath(@__DIR__, ".", "examples", f))
-        setdiff!(names, [".DS_Store", "examples_from_esdl_study_4.jl"])
+        setdiff!(names, [".DS_Store", "examples_from_esdl_study_1.jl","examples_from_esdl_study_2.jl","examples_from_esdl_study_3.jl","examples_from_esdl_study_4.jl", "create_from_func.jl"])
         fpaths = "$(f)/" .* names
         srcsfiles = vcat(srcsfiles, fpaths...)
     end
