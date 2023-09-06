d8131f6a46f869ae5de007cd62d46c2ae1f8ce77
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-09-06 15:36:32 +0200

    Remove subsetcube function
    This function is not used anymore,
    because the indexing is now handled in DimensionalData.

--- src/DatasetAPI/Datasets.jl
+++ src/DatasetAPI/Datasets.jl
@@ -1,5 +1,5 @@
 module Datasets
-import ..Cubes: Cubes, YAXArray, concatenatecubes, CleanMe, subsetcube, copy_diskarray, setchunks, caxes
+import ..Cubes: Cubes, YAXArray, concatenatecubes, CleanMe, copy_diskarray, setchunks, caxes
 using ...YAXArrays: YAXArrays, YAXDefaults, findAxis
 using DataStructures: OrderedDict, counter
 using Dates: Dates, Day, Hour, Minute, Second, Month, Year, Date, DateTime, TimeType, AbstractDateTime, Period
@@ -502,7 +502,6 @@ function open_dataset(g; skip_keys=(), driver=:all, force_datetime=false, path=n
         Dataset(allcubes, sdimlist,gatts)
     end
 end
-#Base.getindex(x::Dataset; kwargs...) = subsetcube(x; kwargs...)
 YAXDataset(; kwargs...) = Dataset(YAXArrays.YAXDefaults.cubedir[]; kwargs...)
 
 
