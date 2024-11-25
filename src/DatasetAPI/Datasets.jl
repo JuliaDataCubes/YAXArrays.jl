7458386c8256ca4450827d75eaebaee8861abba7
Lazaro Alonso <lazarus.alon@gmail.com>
2024-11-25 23:29:57 +0100

    Apply suggestions from code review

--- src/DatasetAPI/Datasets.jl
+++ src/DatasetAPI/Datasets.jl
@@ -359,7 +359,7 @@ end
 """
     open_mfdataset(files::DD.DimVector{<:AbstractString}; kwargs...)
 
-Opens an concatenates a list of dataset paths along the dimension specified in `files`. 
+Opens and concatenates a list of dataset paths along the dimension specified in `files`. 
 This method can be used when the generic glob-based version of open_mfdataset fails
 or is too slow. 
 For example, to concatenate a list of annual NetCDF files along the `Ti` dimension, 
@@ -376,8 +376,6 @@ dimension provided in the input arg is used:
 ````julia
 files = ["a.nc","b.nc","c.nc"]
 open_mfdataset(DD.DimArray(files,DD.Dim{:NewDim}(["a","b","c"])))
-
-"""
 function open_mfdataset(vec::DD.DimVector{<:AbstractString};kwargs...)
     alldatasets = open_dataset.(vec;kwargs...);
     fi = first(alldatasets)
