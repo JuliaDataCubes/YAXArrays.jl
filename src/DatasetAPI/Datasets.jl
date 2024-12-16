18ff0df114cf094ba11543127aa4c3dfb4a31a3c
Lazaro Alonso <lazarus.alon@gmail.com>
2024-12-16 19:53:22 +0100

    adds docstring

--- src/DatasetAPI/Datasets.jl
+++ src/DatasetAPI/Datasets.jl
@@ -407,10 +407,21 @@ end
 
 
 """
-    open_dataset(g; driver=:all)
+    open_dataset(g; skip_keys=(), driver=:all)
 
 Open the dataset at `g` with the given `driver`.
 The default driver will search for available drivers and tries to detect the useable driver from the filename extension.
+
+### Keyword arguments
+
+- `skip_keys` are passed as symbols, i.e., `skip_keys = (:a, :b)`
+- `driver=:all`, common options are `:netcdf` or `:zarr`.
+
+Example:
+
+````julia
+ds = open_dataset(f, driver=:zarr, skip_keys = (:c,))
+````
 """
 function open_dataset(g; skip_keys=(), driver = :all)
     str_skipkeys = string.(skip_keys)
