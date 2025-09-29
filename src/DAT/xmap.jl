eac56ce83a63886fc276fd2fac33850622438ee7
Fabian Gans <fgans@bgc-jena.mpg.de>
2025-09-29 17:31:55 +0200

    fix #533

--- src/DAT/xmap.jl
+++ src/DAT/xmap.jl
@@ -183,7 +183,7 @@ struct XOutput{D<:Tuple{Vararg{DD.Dimension}},T}
     outtype::T
     properties
 end
-function XOutput(outaxes...; outtype=1,properties=Dict())
+function XOutput(outaxes::DD.Dimension...; outtype=1, properties=Dict())
     XOutput(outaxes, outtype,properties)
 end
 
@@ -246,26 +246,22 @@ function xmap(f, ars::Union{YAXArrays.Cubes.YAXArray,DimWindowArray}...;
     if output isa XOutput
         output = (output,)
     end
+
+    alloutdims = mapreduce(approxunion!, output, init=[]) do o
+        o.outaxes
+    end
+
+    allinandoutdims = (unique(DD.basedims((alldims..., alloutdims...)))...,)
+
+
     outaxinfo = map(output) do o
-        nout_old = length(o.outaxes)
-        n_added = -1
-        r = map(o.outaxes) do ax
-            ax_indim = DD.dims(alldims,ax)
-            if isnothing(ax_indim)
-                n_added += 1
-                ax, nout_old + length(alldims) + n_added
-            else
-                idim = DD.dimnum(alldims,ax)
-                ax, idim
-            end
-        end
-        outaxes = map(first,r)
-        dimsmap = map(last,r)
-        addaxes = DD.otherdims(alldims,DD.basedims(outaxes))
-        dimsmapadd = setdiff(ntuple(identity,length(alldims)),dimsmap)
+        outaxes = o.outaxes
+        addaxes = DD.otherdims(alldims, DD.basedims(outaxes))
         outwindows = map(i->[Base.OneTo(length(i))],outaxes)
         extrawindows = Base.OneTo.(length.(addaxes))
-        (outaxes...,addaxes...), (dimsmap...,dimsmapadd...), (outwindows...,extrawindows...)
+        alloutaxes = (outaxes..., addaxes...)
+        dimsmap = DD.dimnum(allinandoutdims, alloutaxes)
+        alloutaxes, dimsmap, (outwindows..., extrawindows...)
     end
     outaxes = map(first,outaxinfo)
     dimsmap = map(Base.Fix2(getindex,2),outaxinfo)
