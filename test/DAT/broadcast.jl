d1f920a806d6889b2a8a479440f433961e5cc518
Felix Cremer <fcremer@bgc-jena.mpg.de>
2026-04-01 16:05:14 +0200

    Make xmap eager for in memory data by default (#583)
    * Fix wrong docstring ending
    
    * Fix disconnected Graphs test
    
    * Adress comments
    
    ---------
    
    Co-authored-by: Fabian Gans <fgans@bgc-jena.mpg.de>

--- test/DAT/broadcast.jl
+++ test/DAT/broadcast.jl
@@ -11,53 +11,61 @@ end
 a, b, c = sample_arrays()
 
 @testset "YAXArray lazy broadcast tests" begin
+    for lazy in (true, false)
+        YAXArrays.Xmap.LAZY_INMEMORY_XMAP[] = lazy
+        # Scalar broadcasting
+        x1 = a .+ 1
+        @test eltype(x1) == eltype(a)
+        @test size(x1) == size(a)
+        @test all(x1[:] .== 2.0)
+        @test DiskArrays.isdisk(x1) == lazy
+        x2 = 1 .+ a
+        @test all(x2[:] .== 2.0)
+        @test DiskArrays.isdisk(x2) == lazy
 
-    # Scalar broadcasting
-    x1 = a .+ 1
-    @test eltype(x1) == eltype(a)
-    @test size(x1) == size(a)
-    @test all(x1[:] .== 2.0)
+        # Element-wise addition
+        xadd = a .+ b
+        @test all(xadd[:] .== 1.0)
+        @test DiskArrays.isdisk(xadd) == lazy
 
-    x2 = 1 .+ a
-    @test all(x2[:] .== 2.0)
+        # Element-wise multiplication
+        xmul = a .* b
+        @test all(xmul[:] .== 0.0)
+        @test DiskArrays.isdisk(xmul) == lazy
+        # Unary functions
+        xneg = -a
+        @test all(xneg[:] .== -1.0)
+        @test DiskArrays.isdisk(xneg) == lazy
+        xabs = abs.(xneg)
+        @test all(xabs[:] .== 1.0)
+        @test DiskArrays.isdisk(xabs) == lazy
+        # Logical / predicates
+        xisnan = isnan.(c)
+        @test eltype(xisnan) == Bool
+        @test xisnan[1,2] == true
+        @test xisnan[2,3] == true
+        @test xisnan[1,1] == false                
+        @test DiskArrays.isdisk(xisnan) == lazy
 
-    # Element-wise addition
-    xadd = a .+ b
-    @test all(xadd[:] .== 1.0)
+        xnotnan = .!isnan.(c)
+        @test xnotnan[1,1] == true
+        @test xnotnan[1,2] == false
+        @test DiskArrays.isdisk(xnotnan) == lazy
 
-    # Element-wise multiplication
-    xmul = a .* b
-    @test all(xmul[:] .== 0.0)
+        # Mixed operations
+        xmix = (a .+ b) .* 2 .- 1
+        @test all(xmix[:] .== 1.0)
 
-    # Unary functions
-    xneg = -a
-    @test all(xneg[:] .== -1.0)
+        # Chained broadcasts
+        xchain = .!isnan.(c .+ 1)
+        @test xchain[1,2] == false
+        @test xchain[1,1] == true
+        @test DiskArrays.isdisk(xchain) == lazy
+        # Mixed operations with numbers
+        xscalar = a .* 3 .+ 1
+        @test all(xscalar[:] .== 4.0)
+        @test DiskArrays.isdisk(xscalar) == lazy
+        @test isa(a .+ b, YAXArray)
 
-    xabs = abs.(xneg)
-    @test all(xabs[:] .== 1.0)
-
-    # Logical / predicates
-    xisnan = isnan.(c)
-    @test eltype(xisnan) == Bool
-    @test xisnan[1,2] == true
-    @test xisnan[2,3] == true
-    @test xisnan[1,1] == false
-
-    xnotnan = .!isnan.(c)
-    @test xnotnan[1,1] == true
-    @test xnotnan[1,2] == false
-
-    # Mixed operations
-    xmix = (a .+ b) .* 2 .- 1
-    @test all(xmix[:] .== 1.0)
-
-    # Chained broadcasts
-    xchain = .!isnan.(c .+ 1)
-    @test xchain[1,2] == false
-    @test xchain[1,1] == true
-
-    # Mixed operations with numbers
-    xscalar = a .* 3 .+ 1
-    @test all(xscalar[:] .== 4.0)
-    @test isa(a .+ b, YAXArray)
+    end
 end
